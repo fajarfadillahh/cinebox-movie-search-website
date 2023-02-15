30334933f30d257b0de97e92c779f32f23d76c17
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-15 21:28:39 +0700

    create: Contents components

---
+++ src/components/Contents.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Contents = () => {
+  return (
+    <section className="section relative pt-40">
+      <div className="container grid gap-24">
+        <div className="text-center">
+          <h1 className="section-title pb-8">
+            Find your favorite movie<span className="text-blue-600">.</span>
+          </h1>
+          <form className="grid items-center gap-4">
+            <input
+              type="text"
+              placeholder="Search film, series, or tv show..."
+              className="flex h-[55px] items-center bg-gray-200 px-6 font-title font-semibold text-gray-900 outline-none placeholder:font-sans placeholder:font-semibold placeholder:-tracking-[0.9px] placeholder:text-gray-600"
+            />
+            <button className="btn w-full" onClick={(e) => e.preventDefault()}>
+              Search
+            </button>
+          </form>
+        </div>
+
+        <div>movies</div>
+      </div>
+    </section>
+  );
+};
+
+export default Contents;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 // import components
 import Header from "../components/Header";
+import Contents from "../components/Contents";
 
 const Home = () => {
   return (
     <>
-      <div className="-z-50 mx-auto max-w-[1440px] overflow-hidden bg-gray-900">
+      <div className="-z-50 mx-auto max-w-[1440px] overflow-hidden">
         <Header />
+        <Contents />
       </div>
     </>
   );
