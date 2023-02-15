0f459ba9cc862ccd29d9def1cac529fd0a027873
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-15 21:38:54 +0700

    create: Data.jsx

---
+++ src/Data.jsx
@@ -0,0 +1,44 @@
+// import images
+import MovieImg1 from "./assets/images/movie-img-(1).jpg";
+import MovieImg2 from "./assets/images/movie-img-(2).jpg";
+import MovieImg3 from "./assets/images/movie-img-(3).jpg";
+import MovieImg4 from "./assets/images/movie-img-(4).jpg";
+
+export const MovieData = [
+  {
+    id: 1,
+    movieImg: MovieImg1,
+    title: "Stranger Things",
+    rating: 84.0,
+    location: "USA",
+    date_release: "2016",
+    tags: [{ tag: "Action" }, { tag: "Adventure" }, { tag: "Horor" }],
+  },
+  {
+    id: 2,
+    movieImg: MovieImg2,
+    title: "Batman Begins",
+    rating: 84.0,
+    location: "USA",
+    date_release: "2006",
+    tags: [{ tag: "Action" }, { tag: "Adventure" }],
+  },
+  {
+    id: 3,
+    movieImg: MovieImg3,
+    title: "Spider-Man: Into The Spider-Verse",
+    rating: 84.0,
+    location: "USA",
+    date_release: "2018",
+    tags: [{ tag: "Action" }, { tag: "Adventure" }, { tag: "Animations" }],
+  },
+  {
+    id: 4,
+    movieImg: MovieImg4,
+    title: "Dunkrik",
+    rating: 84.0,
+    location: "USA",
+    date_release: "2016",
+    tags: [{ tag: "Action" }, { tag: "Drama" }, { tag: "History" }],
+  },
+];
