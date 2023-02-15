d58f47ca3f69ebcdad11dbfa436ae7b3b4d0f601
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-15 22:25:16 +0700

    create: Footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  RiYoutubeFill,
+  RiTwitterFill,
+  RiInstagramFill,
+  RiGithubFill,
+} from "react-icons/ri";
+
+const Footer = () => {
+  return (
+    <footer className="relative pt-24 pb-8">
+      <div className="container grid justify-items-center gap-8">
+        <div className="flex items-center gap-6">
+          {[
+            [<RiYoutubeFill />, "/"],
+            [<RiTwitterFill />, "/"],
+            [<RiInstagramFill />, "/"],
+            [<RiGithubFill />, "/"],
+          ].map(([title, url, index]) => {
+            return (
+              <Link
+                key={index}
+                to={url}
+                className="inline-flex text-[1.8rem] font-medium text-gray-900 hover:text-blue-600"
+              >
+                {title}
+              </Link>
+            );
+          })}
+        </div>
+
+        <div className="flex flex-wrap items-center justify-center gap-4">
+          {[
+            ["Conditions of Use", "/"],
+            ["Privacy & Policy", "/"],
+            ["Press Room of Use", "/"],
+          ].map(([title, url, index]) => {
+            return (
+              <Link
+                key={index}
+                to={url}
+                className="font-medium text-gray-900 hover:text-blue-600 hover:underline"
+              >
+                {title}
+              </Link>
+            );
+          })}
+        </div>
+
+        <p className="section-text">&copy; Cinebox. | by Fajar Fadillah A</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 // import components
 import Header from "../components/Header";
 import Contents from "../components/Contents";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
@@ -10,6 +11,8 @@ const Home = () => {
       <div className="-z-50 mx-auto max-w-[1440px] overflow-hidden">
         <Header />
         <Contents />
+        <div className="h-[1000px]" />
+        <Footer />
       </div>
     </>
   );
