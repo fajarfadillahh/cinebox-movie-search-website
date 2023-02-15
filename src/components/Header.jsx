f47f757c2626c1404987c24e6302daa054525fee
fajarfadillahh <fajaragustian979@gmail.com>
2023-02-15 22:32:40 +0700

    modified: add bg-color at the Header components when scrolling down

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [stickyHeader, setStickyHeader] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 5) {
+        setStickyHeader(true);
+      } else {
+        setStickyHeader(false);
+      }
+    });
+  }, []);
+
   return (
-    <header className="fixed top-0 z-50 mx-auto w-full max-w-[1440px]">
+    <header
+      className={`fixed top-0 z-50 mx-auto w-full max-w-[1440px] transition ${
+        stickyHeader ? "bg-white" : "bg-transparent"
+      }`}
+    >
       <div className="container flex h-24 items-center justify-center border-b-2 border-gray-300">
         <Link to="/" className="section-title text-[24px]">
           Cinebox<span className="text-blue-600">.</span>
